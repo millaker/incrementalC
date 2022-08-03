815b91fd17f60995af8ecce739c155a2019d0a5a
Jacob Lin <lovetaeyeon507@gmail.com>
2022-08-04 02:47:36 +0800

    Add tests for invalid c code

--- test_week1.sh
+++ test_week1.sh
@@ -22,3 +22,24 @@ do
     rm $base
 done
 rm a.out
+
+for i in week1/invalid/*.c
+do
+    ./test.out $i 2> /dev/null
+    base=${i%.c}.s
+    echo -n "$i: "
+    flag=0
+    for j in week1/invalid/*
+    do
+        if [ "$base" == "$j" ]
+        then
+            echo "FAIL"
+            flag=1
+            break
+        fi
+    done
+    if [ $flag != 1 ]
+    then
+        echo "PASS"
+    fi
+done
